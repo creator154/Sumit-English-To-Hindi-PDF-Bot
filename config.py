5c591c1a8ff08b8118cc625a42c5b3a3509644b5
creator154 <cteator898@gmail.com>
2026-09-13 09:30:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
-hereimport os
+import os
 
 API_ID = int(os.environ.get("API_ID", "0"))
 API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-GOOGLE_APPLICATION_CREDENTIALS = os.environ.get(
-    "GOOGLE_APPLICATION_CREDENTIALS",
-    ""
-)
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
 DOWNLOAD_DIR = "downloads"
 OUTPUT_DIR = "output"
+FONT_PATH = "NotoSansDevanagari-Regular.ttf"
